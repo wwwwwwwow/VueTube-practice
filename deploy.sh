fb6f0e76b7a9fd756a0e1237c3303e6374ccd493
wwwwwwwow <wxxyayxxw@gmail.com>
2020-03-15 02:10:08 +0800

    Setup project

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:wwwwwwwow/VueTube-practice.git master:gh-pages
+git push -f https://github.com/wwwwwwwow/VueTube-practice.git master:gh-pages
 
 cd -
